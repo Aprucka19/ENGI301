72db6481d6b8983f4bd7b07d7aba0a134ef5ebad
Aprucka19 <awp@prucka.com>
2022-03-28 13:16:49 -0500

    Update documentation

--- BTRobotCode/Motor.py
+++ BTRobotCode/Motor.py
@@ -1,3 +1,41 @@
+"""
+--------------------------------------------------------------------------
+Motor.py
+--------------------------------------------------------------------------
+License:   
+Copyright 2022 <Alex Prucka>
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this 
+list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+
+An interface class for using a DC motor. Takes the enable and the two direction
+pins as initilization values
+
+Requirements:
+  - Given GPIOs for enable, in1, and in2
+Uses:
+  - pigpio
+"""
+
+
 from time import sleep
 import pigpio
 
@@ -14,9 +52,11 @@ class Motor():
 		self.IN1 = IN1
 		self.IN2 = IN2
 		
+		#set as outputs
 		pi.set_mode(self.EN, pigpio.OUTPUT)
 		pi.set_mode(self.IN1, pigpio.OUTPUT)
 		pi.set_mode(self.IN2, pigpio.OUTPUT)
+		#Set initial values
 		pi.write(self.EN, 0)
 		pi.write(self.IN1, 1)
 		pi.write(self.IN2, 0)
